fc004f3b4d629b46cb225b991f86993674f5c181
Adrien Czerny <adrien.czerny@gmail.com>
2020-03-16 19:33:30 +0000

    Renamed; added vim bootstrapping

---
+++ vim.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# install plug
+echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install vundle
+echo "git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install pathogen
+echo "mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim"
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# download landscape colors
+echo "mkdir -p ~/.vim/colors"
+mkdir -p ~/.vim/colors
+echo "wget https://raw.githubusercontent.com/itchyny/landscape.vim/master/colors/landscape.vim -o ~/.vim/colors/"
+wget https://raw.githubusercontent.com/itchyny/landscape.vim/master/colors/landscape.vim -o ~/.vim/colors/landscape.vim
