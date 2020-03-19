df6366cb3f66da61566c61119757c7dd09636bea
Adrien Czerny <adrien.czerny@gmail.com>
2020-03-19 22:57:37 +0000

    Added pulseaudio bluetooth and systrays

--- pacman.sh
+++ pacman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Needs to be run as root; however controversial this might or might not be :shrug:
 
-PACMAN_PACKAGES="fd tmux exa fzf bat the_silver_searcher wget termite openssh picom xclip alsa-utils feh networkmanager-openconnect"
+PACMAN_PACKAGES="fd tmux exa fzf bat feh the_silver_searcher jq unzip htop redshift wget termite openssh picom xclip alsa-utils networkmanager-openconnect network-manager-applet pulseaudio pulseaudio-alsa pulseaudio-jack pulseaudio-bluetooth bluez-utils pasystray"
 echo "pacman -Sy $PACMAN_PACKAGES"
 pacman -Sy $PACMAN_PACKAGES
 
